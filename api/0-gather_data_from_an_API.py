1162bbfc7958832d216c3aafb8fd39a6704caf91
Maykha27 <5144@holbertonschool.com>
2023-01-02 13:46:57 +0100

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
-
 """
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+a Python script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress.
 """
-
 from requests import get
 from sys import argv as arg
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+ # create api_todos dictionary
     api_todos_dict = {'userId': arg}
-    response = get('https://jsonplaceholder.typicode.com/todos',params=api_todos_dict)
-    data = response.json()
+    # and add it to the url
+    todos_url = get('https://jsonplaceholder.typicode.com/todos', params=api_todos_dict)
+    todos = todos_url.json()
+# create api_user dictionary
+    api_user_dict = {'id': arg}
+# add it to url
+    user_url = get('https://jsonplaceholder.typicode.com/users',params=api_user_dict)
+    user = user_url.json()
+# define the variables
+    EMPLOYEE_NAME = None
     completed = 0
-    total = 0
-    tasks = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
-
-    for i in data2:
-        if i.get('id') == int(arg[1]):
-            Employee = i.get('Name')
-
-    for i in data:
-        if i.get('userId') == int(arg[1]):
-            total += 1
-
-            if i.get('completed') is True:
-                completed += 1
-                tasks.append(i.get('title'))
-
-    print("Employee {} is done with tasks({}/{}):".format(Employee, completed,
-                                                          total))
-
-    for i in tasks:
-        print("\t {}".format(i))
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS_title = []
+# find the value of the employee_name
+    EMPLOYEE_NAME = user[0].get('name')
+# The script must accept an integer as a parameter which is the employee ID
+    for j in todos:
+      if j.get('userId') == int(arg[1]):
+# to add total number of tasks
+            TOTAL_NUMBER_OF_TASKS += 1
+            if j.get('completed') is True:
+# to add number of done tasks
+               completed += 1
+               NUMBER_OF_DONE_TASKS_title.append(j.get('title'))
+print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, completed,
+                                                   TOTAL_NUMBER_OF_TASKS))
+for i in NUMBER_OF_DONE_TASKS_title:
+             print("{} {}".format('\t', i))
