dd8a2592359083bd86e69f2e280b8e8d10d31408
Maykha27 <5144@holbertonschool.com>
2023-01-02 18:33:55 +0100

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 """
-place holder
+uses REST API to gather data for employee to-do list
+and exports information in CSV format to new file
 """
-
-
 if __name__ == "__main__":
-
-    import csv
     import requests
     from sys import argv
+    import csv
 
-    if len(argv) < 2:
-        exit()
-    todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}"
-        .format(argv[1]))
-    name = requests.get(
-        "https://jsonplaceholder.typicode.com/users?id={}".format(argv[1]))
-    name = name.json()
-    name = name[0]["username"]
-    todos = todos.json()
-    file_name = "{}.csv".format(argv[1])
-    with open(file_name, 'w') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            writer.writerow([argv[1], name, todo['completed'], todo['title']])
+    user_URL = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    employee = requests.get(user_URL).json()
+    employ_username = employee.get('username')
+
+    tasks_URL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        argv[1])
+    all_tasks = requests.get(tasks_URL).json()
+
+    filename = "{}.csv".format(argv[1])
+    with open(filename, 'w') as f:
+        f.write("")
+    for task in all_tasks:
+        with open(filename, 'a', newline='') as f:
+            csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            values = []
+            values.append("{}".format(argv[1]))
+            values.append("{}".format(employ_username))
+            values.append("{}".format(task.get('completed')))
+            values.append("{}".format(task.get('title')))
+            csv_writer.writerow(values)
